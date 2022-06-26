1d4281a02c9014cd34e3e39ded49242c20608c94
Junaid <zulfiqar.junaid95@gmail.com>
2022-06-25 18:38:25 -0700

    Updates

---
+++ precache-manifest.bbd2bc2039dbe60c67726087919b60ae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3dee70bc150d7166b285071e1eddd3d",
+    "url": "/Stripe-Menu/index.html"
+  },
+  {
+    "revision": "be3e2240913b09153312",
+    "url": "/Stripe-Menu/static/css/main.7215cb73.chunk.css"
+  },
+  {
+    "revision": "e573c7512bb063759c33",
+    "url": "/Stripe-Menu/static/js/2.64a39709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Stripe-Menu/static/js/2.64a39709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be3e2240913b09153312",
+    "url": "/Stripe-Menu/static/js/main.cbd59232.chunk.js"
+  },
+  {
+    "revision": "0360633e373c25d60391",
+    "url": "/Stripe-Menu/static/js/runtime-main.175f422b.js"
+  },
+  {
+    "revision": "882a211ad0bae36b9c11715c9291f36b",
+    "url": "/Stripe-Menu/static/media/hero.882a211a.svg"
+  },
+  {
+    "revision": "86d618b5be1508bcbbb31d6393f089f6",
+    "url": "/Stripe-Menu/static/media/logo.86d618b5.svg"
+  },
+  {
+    "revision": "e908706c41907a9611ba9c4d61a32f7c",
+    "url": "/Stripe-Menu/static/media/phone.e908706c.svg"
+  }
+]);
